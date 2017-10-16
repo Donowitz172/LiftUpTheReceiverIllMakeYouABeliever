1d3ad73911b6664d9753690ee01860c312572566
Donowitz172 <malesevicviktor@gmail.com>
2017-10-16 16:43:41 +0200

    Upload basic draw_names

---
+++ R/draw_names.R
@@ -0,0 +1,34 @@
+#' Function drawing the evolution of multiple given names in France by year
+#'
+#' @param name_vector vector of names to be drawn
+#'
+#' @import dplyr
+#' @import ggplot2
+#' @import ggthemes
+#'
+#' @return ggplot of names
+#' @export
+#'
+#' @examples
+#'
+#' draw_names(c("Viktor","Jacques"))
+
+draw_names <- function(name_vector){
+
+  l <- prenoms::prenoms %>%
+
+    filter(name %in% name_vector) %>%
+
+    group_by(year,name) %>%
+
+    summarize(number_of_people = sum(n)) %>%
+
+    ggplot(aes(x = year,y = number_of_people, color = name)) +
+
+    geom_line() +
+
+    theme_gdocs()
+
+  return(l)
+
+}
